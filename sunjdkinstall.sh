40d77cde9a6af965920a2a31fe483f59fa7a7113
ishellstrike <ishellstrike@gmail.com>
2013-09-20 22:29:26 +0400

    Update sunjdkinstall.sh

--- sunjdkinstall.sh
+++ sunjdkinstall.sh
@@ -6,5 +6,5 @@ sudo apt-get purge openjdk*
 echo "Instaling sunjdk"
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
+sudo apt-get install oracle-java6-installer
 echo "Sun JDK installed"
